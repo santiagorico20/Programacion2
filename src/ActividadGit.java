6ab174de4eb58153de2af9f94174a8ce708a3b83
jhoan <jhoans.gonzalezu@uqvirtual.edu.co>
2026-02-13 15:13:33 -0500

    cambio

--- src/ActividadGit.java
+++ src/ActividadGit.java
@@ -1,5 +1,6 @@
 public class ActividadGit {
-        public static void main(String[] args) {
+        public static void main(String[] args)
+        {
             System.out.println("Subir Actividad");
         }
 }
